89bfad4105354a933274beb6021d6105b6263246
jayaraman <95jayaraman.r63@gmail.com>
2026-03-09 18:03:58 +0530

    fix: enhance spare part logic and update test cases

--- quickfix/service_center/doctype/spare_part/spare_part.py
+++ quickfix/service_center/doctype/spare_part/spare_part.py
@@ -1,18 +1,24 @@
 import frappe
 from frappe.model.document import Document
 
+
 class SparePart(Document):
+	def autoname(self):
+		if not self.part_code:
+			frappe.throw("Part Code is required")
 
-    def autoname(self):
-        if not self.part_code:
-            frappe.throw("Part Code is required")
+		code = self.part_code.upper()
 
-        code = self.part_code.upper()
+		series = frappe.model.naming.make_autoname("PART-.YYYY.-.####")
 
-        series = frappe.model.naming.make_autoname("PART-.YYYY.-.####")
+		self.name = f"{code}-{series}"
 
-        self.name = f"{code}-{series}"
+	def validate(self):
+		if self.selling_price <= self.unit_cost:
+			frappe.throw("Selling Price must be greater than Unit Cost")
 
-    def validate(self):
-        if self.selling_price <= self.unit_cost:
-            frappe.throw("Selling Price must be greater than Unit Cost")
+	def on_update(self):
+		threshold = frappe.db.get_value("QuickFix Settings", None, "low_stock_threshold")
+
+		if threshold and self.stock_qty < threshold:
+			frappe.msgprint("Stock below threshold")
